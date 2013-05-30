ad0a1e700bf4675b365e50e2953e5c9257bc8abd
Christian Tietze <christian.tietze@gmail.com>
2013-05-30 11:48:12 +0200

    implements favicons

--- header.php
+++ header.php
@@ -6,6 +6,14 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 <title><?php hybrid_document_title(); ?></title>
 <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/images/apple.png">
+<link rel="icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.png">
+<!--[if IE]><link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico"><![endif]-->
+<!-- or, set /favicon.ico for IE10 win -->
+<meta name="msapplication-TileColor" content="#F5F5F5">
+<meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/images/tileicon.png">
+
 <link href='http://fonts.googleapis.com/css?family=Oswald:700&subset=latin,latin-ext' rel='stylesheet' type='text/css' />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
