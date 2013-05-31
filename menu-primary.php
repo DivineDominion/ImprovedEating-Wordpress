ed51e0159e7fcba1f9288dca2a9e81ccd25742b1
Christian Tietze <christian.tietze@gmail.com>
2013-05-31 15:02:01 +0200

    (should) include and style MSLS menu items

--- menu-primary.php
+++ menu-primary.php
@@ -1,5 +1,9 @@
 <?php if ( has_nav_menu( 'primary' ) ) {
-
+    $lang = "";
+    if (function_exists("the_msls")) {
+        $lang = the_msls();
+    }
+    
 	wp_nav_menu(
 		array(
 			'theme_location'  => 'primary',
@@ -7,9 +11,9 @@
 			'container_id'    => 'menu-primary',
 			'container_class' => 'menu',
 			'menu_id'         => 'menu-primary-items',
-			'menu_class'      => 'menu-items',
+			'menu_class'      => 'menu-items nav',
 			'fallback_cb'     => '',
-			'items_wrap'      => '<h3 class="menu-toggle" title="' . esc_attr__( 'Navigation', 'seamless' ) . '">' . __( 'Navigation', 'seamless' ) . '</h3><div class="wrap"><ul id="%1$s" class="%2$s">%3$s</ul></div>'
+			'items_wrap'      => '<h3 class="menu-toggle" title="' . esc_attr__( 'Navigation', 'seamless' ) . '">' . __( 'Navigation', 'seamless' ) . '</h3><div class="wrap"><ul id="%1$s" class="%2$s">%3$s</ul>' . $lang . '</div>'
 		)
 	);
 
