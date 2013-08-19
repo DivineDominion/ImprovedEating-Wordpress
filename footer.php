55533325103defa74a6992c4478dc735cbcad928
Christian Tietze <christian.tietze@gmail.com>
2013-08-19 19:17:07 +0200

    newsletter sidebar added

--- footer.php
+++ footer.php
@@ -1,29 +1,29 @@
-			<?php get_sidebar( 'primary' ); // Loads the sidebar-primary.php template. ?>
+            <?php get_sidebar( 'primary' ); // Loads the sidebar-primary.php template. ?>
 
-		</div><!-- #main -->
+        </div><!-- #main -->
 
-		<?php get_sidebar( 'subsidiary' ); // Loads the sidebar-subsidiary.php template. ?>
+        <?php get_sidebar( 'subsidiary' ); // Loads the sidebar-subsidiary.php template. ?>
 
-		<?php get_template_part( 'menu', 'subsidiary' ); // Loads the menu-subsidiary.php template. ?>
+        <?php get_template_part( 'menu', 'subsidiary' ); // Loads the menu-subsidiary.php template. ?>
 
-		<footer id="footer">
+        <footer id="footer">
 
-			<div class="wrap">
+            <div class="wrap">
 
-				<div class="footer-content">
+                <div class="footer-content">
                     <small class="license">
                         <?php echo apply_atomic_shortcode( 'footer_content', '<p class="copyright">' . __( 'Except where otherwise noted, content on this site is licensed under a <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" title="Creative Commons &mdash; Attribution-NonCommercial-ShareAlike 3.0 Unported
                           &mdash; CC BY-NC-SA 3.0">Creative Commons Attribution-NonCommercial-ShareAlike License</a>.', 'seamless' ) . '</p>' . '<p class="credit">' . __( 'Powered by [wp-link] and [theme-link].', 'seamless' ) . '</p>' ); ?>
                   </small>
-				</div><!-- .footer-content -->
+                </div><!-- .footer-content -->
 
-			</div>
+            </div>
 
-		</footer><!-- #footer -->
+        </footer><!-- #footer -->
 
-	</div><!-- #container -->
+    </div><!-- #container -->
 
-	<?php wp_footer(); // wp_footer ?>
+    <?php wp_footer(); // wp_footer ?>
 
 </body>
 </html>
--- sidebar-primary.php
+++ sidebar-primary.php
@@ -1,9 +1,21 @@
 <?php if ( is_active_sidebar( 'primary' ) ) { ?>
 
-	<aside id="sidebar-primary" class="sidebar">
+    <aside id="newsletter" class="sidebar">
+        <h3 class="widget-title">News</h3>
+        
+        <div id="mc_embed_signup">
+            <form action="http://improvedeating.us7.list-manage1.com/subscribe/post?u=7561c4382788637203e9b973e&amp;id=d6395d2dc2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <label for="mce-EMAIL">Abonnenten sind smart und sexy:</label>
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-Mail Adresse" required><br>
+                <input type="submit" value="Anmelden" name="subscribe" id="mc-embedded-subscribe" class="button">
+            </form>
+        </div>
+    </aside>
+    
+    <aside id="sidebar-primary" class="sidebar">
 
-		<?php dynamic_sidebar( 'primary' ); ?>
+        <?php dynamic_sidebar( 'primary' ); ?>
 
-	</aside><!-- #sidebar-primary .aside -->
+    </aside><!-- #sidebar-primary .aside -->
 
 <?php } ?>
