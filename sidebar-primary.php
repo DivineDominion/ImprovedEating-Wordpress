aed7731f9c4bccd963869ccdcac464f7d881bc2f
Christian Tietze <christian.tietze@gmail.com>
2013-08-20 14:12:43 +0200

    newsletter styled;  background darkened

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -8,6 +8,8 @@
                 <label for="mce-EMAIL">Abonnenten sind smart und sexy:</label>
                 <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="E-Mail Adresse" required><br>
                 <input type="submit" value="Anmelden" name="subscribe" id="mc-embedded-subscribe" class="button">
+
+                <p><a href="http://us7.campaign-archive1.com/home/?u=7561c4382788637203e9b973e&id=d6395d2dc2" title="View previous campaigns">View previous campaigns.</a></p>
             </form>
         </div>
     </aside>
